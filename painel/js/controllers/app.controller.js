85a5fb1a0d9088aeca6ed449f0c05c0bf297bbe8
Lucas <lucashen@gmail.com>
2017-01-23 21:26:57 -0200

    https://github.com/LucasHenriqueOliveira/snack4me_admin/issues/24

--- painel/js/controllers/app.controller.js
+++ painel/js/controllers/app.controller.js
@@ -20,6 +20,18 @@
         if(!$localstorage.get('email') && !$localstorage.get('token') && !$localstorage.get('id')){
             $scope.logged = false;
         }
+
+        $scope.logout = function() {
+            $localstorage.remove('id');
+            $localstorage.remove('email');
+            $localstorage.remove('token');
+            $localstorage.remove('roles_id');
+            $localstorage.remove('roles_name');
+            $localstorage.remove('login_default');
+            $localstorage.remove('company');
+
+            window.location.href = 'http://www.snack4me.com/hotel/admin/';
+        };
     }
 
 })();
